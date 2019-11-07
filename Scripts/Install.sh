4df4f9b58f38bb0a05a19f5e45932008cebcc0bc
badmusishaq <badmusishaq@gmail.com>
2019-11-07 18:44:47 +0100

    use ios env

--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -3,8 +3,8 @@
 # See https://unity3d.com/get-unity/download/archive
 # to get download URLs
 # UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
-# UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/20c1667945cf/MacEditorInstaller/Unity-2019.2.0f1.pkg"
-UNITY_WINDOWS_TARGET_PACKAGE_URL="https://beta.unity3d.com/download/20c1667945cf/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2019.2.0f1.pkg"
+UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/20c1667945cf/MacEditorInstaller/Unity-2019.2.0f1.pkg"
+# UNITY_WINDOWS_TARGET_PACKAGE_URL="https://beta.unity3d.com/download/20c1667945cf/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2019.2.0f1.pkg"
 
 # function toget file
 
@@ -16,7 +16,7 @@ getFileName() {
 download() {
 
 	file=$1
-	URL="$UNITY_WINDOWS_TARGET_PACKAGE_URL"
+	URL="$UNITY_OSX_PACKAGE_URL"
 	 filePath=$(getFileName $file)
    	 fileName=`basename "$file"`
 	
